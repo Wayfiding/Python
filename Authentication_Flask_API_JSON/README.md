ef817d8366e02ed2f72d659c8e5ca10e0b72f587
alberto <albertojr11@hotmail.com>
2021-10-09 20:20:51 -0300

    API studies added

---
+++ Authentication_Flask_API_JSON/README.md
@@ -0,0 +1,19 @@
+<h1 align="center">Welcome to Authentication Flask API JSON 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-Launch Base-blue.svg?cacheSeconds=2592000" />
+</p>
+
+> API studies testing
+
+## Author
+
+👤 **Alberto Junior**
+
+* Github: [@Wayfiding](https://github.com/Wayfiding)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ Authentication_Flask_API_JSON/api.py
@@ -0,0 +1,49 @@
+from flask import Flask, jsonify, request, make_response
+import jwt 
+import datetime
+from functools import wraps
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'thisisthesecretkey'
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token') #http://127.0.0.1:5000/route?token=alshfjfjdklsfj89549834ur
+        print(token)
+        if not token:
+            return jsonify({'message' : 'Token is missing!'}), 403
+
+        try: 
+            data = jwt.decode(token, app.config['SECRET_KEY'],algorithms=["HS256"])
+        except:
+            return jsonify({'message' : 'Token is invalid!'}), 403
+
+        return f(*args, **kwargs)
+       
+
+    return decorated
+
+@app.route('/unprotected')
+def unprotected():
+    return jsonify({'message' : 'Anyone can view this!'})
+
+@app.route('/protected')
+@token_required
+def protected():
+    return jsonify({'message' : 'This is only available for people with valid tokens.'})
+
+@app.route('/login')
+def login():
+    auth = request.authorization
+
+    if auth and auth.password == 'password':
+        token = jwt.encode({'user' : auth.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(seconds=150)}, app.config['SECRET_KEY'])
+
+        return jsonify({'token' : token})
+
+    return make_response('Could not verify!', 401, {'WWW-Authenticate' : 'Basic realm="Login Required"'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
