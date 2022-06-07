5a61e9ab95a0ebfe3d611a76d95bf9d757ae6ce9
alberto <albertojr11@hotmail.com>
2022-06-07 00:34:03 -0300

    Added PythonSockets

---
+++ Python-Sockets/multiconn-client.py
@@ -0,0 +1,73 @@
+import sys
+import socket
+import selectors
+import types
+
+sel = selectors.DefaultSelector()
+messages = [b"Message 1 from client.", b"Messages 2 from client."]
+
+host, port, num_conns = sys.argv[1], int(sys.argv[2]), int(sys.argv[3])
+
+
+
+def start_connections(host, port, num_conns):
+    server_addr = (host, port)
+    for i in range(0, num_conns):
+        connid = i + 1
+        print(f"Starting connection {connid} to {server_addr}")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(False)
+        sock.connect_ex(server_addr)
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        data = types.SimpleNamespace(
+            connid=connid,
+            msg_total=sum(len(m) for m in messages),
+            recv_total=0,
+            messages=messages.copy(),
+            outb=b"",
+        )
+        
+        sel.register(sock,events, data=data)
+        
+def accept_wrapper(sock):
+    conn, addr = sock.accept() # Should be ready to read
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn,events, data=data)
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            print(f"Received {recv_data!r} from connection {data.connid}")
+            data.recv_total += len(recv_data)
+        if not recv_data or data.recv_total == data.msg_total:
+            print(f"Closing connection {data.connid}")
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if not data.outb and data.messages:
+            data.outb = data.messages.pop(0)
+        if data.outb:
+            print(f"Sending {data.outb!r} to connection {data.connid}")
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]        
+
+
+
+try:
+    start_connections(host,port,num_conns)
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key,mask)
+except KeyboardInterrupt:
+    print("Caught keyboard Interrupt,exiting")
+finally:
+    sel.close()
